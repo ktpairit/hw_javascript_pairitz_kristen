85344a1e799a64018f3ddba7f3d64630668c3cfc
Kristen Pairitz <ktpairit@gmail.com>
2015-09-25 21:57:23 -0400

    change presidents object

--- js/main.js
+++ js/main.js
@@ -1,47 +1,39 @@
-var president44 {
-	firstName : "Barak",
-	lastName : "Obama",
-	termLength : 2;
-	party : "democratic",
-	yearsOfPresidency : 7,
+var president44 = {
+	firstName: 'Barak',
+	lastName: 'Obama',
+	termLength: 2,
+	party: 'democratic',
+	yearsOfPresidency: 7
 };
 
-var president43 {
-	firstName : "George",
-	lastName : "Bush",
-	termLength : 2;
-	party : "republican",
-	yearsOfPresidency : 8,
+var president43 = {
+	firstName: 'George',
+	lastName: 'Bush',
+	termLength: 2,
+	party: 'republican',
+	yearsOfPresidency: 8
 };
 
-var president42 {
-	firstName : "Bill",
-	lastName : "Clinton",
-	termLength : 2;
-	party : "democratic",
-	yearsOfPresidency : 8,
+var president42 = {
+	firstName: 'Bill',
+	lastName: 'Clinton',
+	termLength: 2,
+	party: 'democratic',
+	yearsOfPresidency: 8
 };
 
-var president41 {
-	firstName : "George",
-	lastName : "Bush Sr",
-	termLength : 2;
-	party : "republican",
-	yearsOfPresidency : 4,
+var president41 = {
+	firstName: 'George',
+	lastName: 'Bush Sr',
+	termLength: 2,
+	party: 'republican',
+	yearsOfPresidency: 4
 };
 
-var president40 {
-	firstName : "Ronald",
-	lastName : "Reagan",
-	termLength : 2;
-	party : "republican",
-	yearsOfPresidency : 8,
+var president40 = {
+	firstName: 'Ronald',
+	lastName: 'Reagan',
+	termLength: 2,
+	party: 'republican',
+	yearsOfPresidency : 8
 };
-
-var presidents = [
-	'president44',
-	'president43',
-	'president42',
-	'president41',
-	'president40'
-];
