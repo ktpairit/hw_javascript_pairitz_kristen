f32d493a835668c6c10e495c5407c58787ae47b6
Kristen Pairitz <ktpairit@gmail.com>
2015-09-25 21:20:35 -0400

    create object listing presidents

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var presiden44 {
+	firstName : "Barak",
+	lastName : "Obama",
+	termLength : 2;
+	party : "democratic",
+	yearsOfPresidency : 7,
+};
+
+var president43 {
+	firstName : "George",
+	lastName : "Bush",
+	termLength : 2;
+	party : "republican",
+	yearsOfPresidency : 8,
+};
+
+var president42 {
+	firstName : "Bill",
+	lastName : "Clinton",
+	termLength : 2;
+	party : "democratic",
+	yearsOfPresidency : 8,
+};
+
+var president41 {
+	firstName : "George",
+	lastName : "Bush Sr",
+	termLength : 2;
+	party : "republican",
+	yearsOfPresidency : 4,
+};
+
+var president40 {
+	firstName : "Ronald",
+	lastName : "Reagan",
+	termLength : 2;
+	party : "republican",
+	yearsOfPresidency : 8,
+};
