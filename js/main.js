5090957501b782411adaf8b7f534d3164e394dcf
Kristen Pairitz <ktpairit@gmail.com>
2015-09-25 22:45:16 -0400

    modify president object, array, and index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var president44 = {
 	firstName: 'Barak',
 	lastName: 'Obama',
@@ -36,4 +37,14 @@ var president40 = {
 	termLength: 2,
 	party: 'republican',
 	yearsOfPresidency : 8
+};
+
+var presidents = {
+	arrayOfPresidents: [
+		{firstName: 'Barak', lastName: 'Obama', termLength: 2, party: 'democratic', yearsOfPresidency: 7};
+		{firstName: 'George', lastName: 'Bush', termLength: 2, party: 'republican', yearsOfPresidency: 8};
+		{firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'democratic', yearsOfPresidency: 8};
+		{firstName: 'George', lastName: 'Bush Sr', termLength: 2, party: 'republican', yearsOfPresidency: 4};
+		{firstName: 'Ronald', lastName: 'Reagan', termLength: 2, party: 'republican', yearsOfPresidency : 8};
+	];	
 };
