6a007293c9116526014902343ea5eee37cabc247
Kristen Pairitz <ktpairit@gmail.com>
2015-09-26 00:11:48 -0400

    modify console.log code for president object

--- js/main.js
+++ js/main.js
@@ -46,5 +46,9 @@ var presidents = {
 		{firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'democratic', yearsOfPresidency: 8};
 		{firstName: 'George', lastName: 'Bush Sr', termLength: 2, party: 'republican', yearsOfPresidency: 4};
 		{firstName: 'Ronald', lastName: 'Reagan', termLength: 2, party: 'republican', yearsOfPresidency : 8}
-	]	
+	],
+
+	function consoleFunction() {
+		console.log( 'Bill CLinton' );
+	},
 };
